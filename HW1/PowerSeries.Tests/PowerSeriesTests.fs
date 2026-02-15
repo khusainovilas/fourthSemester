67d6825748b49dd1e6a0c9b5a9d0098351d1533c
khusainovilas <khusainovilas@yandex.ru>
2026-02-15 17:54:42 +0300

    Add PowerSeries function and tests

---
+++ HW1/PowerSeries.Tests/PowerSeriesTests.fs
@@ -0,0 +1,22 @@
+﻿module PowerSeriesTests
+
+open NUnit.Framework
+open FsUnit
+open PowerSeries
+
+[<Test>]
+let PowerSeries_PositiveNumbers_ReturnsCorrectList () =
+    powerSeries 0 3 |> should equal ([1; 2; 4; 8])
+    powerSeries 2 4 |> should equal ([4; 8; 16; 32; 64])
+
+[<Test>]
+let PowerSeries_Zero_ReturnsOne () =
+    powerSeries 0 0 |> should equal ([1])
+
+[<Test>]
+let PowerSeries_ZeroM_ReturnsCorrectNumber () =
+    powerSeries 10 0 |> should equal ([1024])
+
+[<Test>]
+let PowerSeries_NegativeNumbers_ReturnsEmptyList () =
+    powerSeries 1 -5 |> should be Empty
---
+++ HW1/PowerSeries/PowerSeries.fs
@@ -0,0 +1,10 @@
+﻿module PowerSeries
+
+let rec powerSeries n m = 
+    if n < 0 || m < 0 then []
+    else 
+        let rec buildSeries cur count list = 
+            if count = 0 then List.rev list 
+            else buildSeries (cur * 2) (count - 1) (cur :: list) 
+
+        buildSeries (pown 2 n) (m + 1) []
