fe10d7765e3692a8d6ac231ec2ce1827e610b67b
khusainovilas <khusainovilas@yandex.ru>
2026-02-15 19:03:30 +0300

    Add SearchNumber and tests

---
+++ HW1/SearchNumber.Tests/SearchNumberTests.fs
@@ -0,0 +1,21 @@
+﻿module SearchNumberTests
+
+open NUnit.Framework
+open FsUnit
+open SearchNumber
+
+[<Test>]
+let SearchNumber_ElementExists () =
+    searchNumber [10; 20; 30; 40] 30 |> should equal 2
+
+[<Test>]
+let SearchNumber_FirstElement () =
+    searchNumber [5; 6; 7] 5 |> should equal 0
+
+[<Test>]
+let SearchNumber_ElementNotFound () =
+    searchNumber [1; 2; 3] 10 |> should equal -1
+
+[<Test>]
+let SearchNumber_EmptyList () =
+    searchNumber [] 5 |> should equal -1
---
+++ HW1/SearchNumber/SearchNumber.fs
@@ -0,0 +1,11 @@
+﻿module SearchNumber
+
+let searchNumber list n = 
+    let rec loop list acc = 
+        match list with 
+        | [] -> -1 
+        | x :: xs -> 
+            match x = n with
+            | true -> acc
+            | false -> loop xs (acc + 1)
+    loop list 0
