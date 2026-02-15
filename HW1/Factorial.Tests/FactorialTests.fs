95bc6c3daf6abd09b358101282b64c1117e8e246
khusainovilas <khusainovilas@yandex.ru>
2026-02-15 16:06:09 +0300

    Add factorial implementation and tests

---
+++ HW1/Factorial.Tests/FactorialTests.fs
@@ -0,0 +1,26 @@
+﻿module FactorialTests
+
+open NUnit.Framework
+open FsUnit
+open Factorial
+
+[<Test>]
+let Factorial_PositiveNumber_ReturnsCorrectResult () =
+    factorial 5 |> should equal (Some 120)
+    factorial 12 |> should equal (Some 479001600)
+
+[<Test>]
+let Factorial_Zero_ReturnsOne () =
+    factorial 0 |> should equal (Some 1)
+
+[<Test>]
+let Factorial_One_ReturnsOne () =
+    factorial 1 |> should equal (Some 1)
+
+[<Test>]
+let Factorial_NegativeNumber_ReturnsNone () =
+    factorial -3 |> should equal None
+
+[<Test>]
+let Factorial_NumberGreaterThanLimit_ReturnsNone () =
+    factorial 13 |> should equal None
---
+++ HW1/Factorial/Factorial.fs
@@ -0,0 +1,10 @@
+﻿module Factorial
+
+let rec factorial n = 
+    match n with
+    | n when n < 0 || n > 12 -> None
+    | 0 -> Some 1
+    | _ -> 
+        match factorial (n - 1) with 
+        | Some value -> Some (n * value)
+        | None -> None
