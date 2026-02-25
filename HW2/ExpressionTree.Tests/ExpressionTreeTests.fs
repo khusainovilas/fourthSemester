8f31902ff874c86a0e63a89e55b234bf175e8f48
khusainovilas <khusainovilas@yandex.ru>
2026-02-25 22:47:09 +0300

    Implement eval function for Expr tree and test

---
+++ HW2/ExpressionTree.Tests/ExpressionTreeTests.fs
@@ -0,0 +1,20 @@
+﻿module ExpressionTree.Tests
+
+open NUnit.Framework
+open FsUnit
+open ExpressionTree
+
+[<Test>]
+let eval_Addition_ReturnsCorrectResult () =
+    let expr = Add(Number 3, Number 5)
+    eval expr |> should equal 8
+
+[<Test>]
+let eval_NestedExpression_ReturnsCorrectResult () =
+    let expr = Mul(Add(Number 3, Number 5), Number 2)
+    eval expr |> should equal 16
+
+[<Test>]
+let eval_SubtractionAndDivision_ReturnsCorrectResult () =
+    let expr = Div(Sub(Number 10, Number 4), Number 2)
+    eval expr |> should equal 3
