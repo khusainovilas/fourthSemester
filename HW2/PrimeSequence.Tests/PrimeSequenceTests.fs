9a4609395fd931672db000b06c53d80cd632e6af
khusainovilas <khusainovilas@yandex.ru>
2026-02-25 20:54:57 +0300

    Implement prime sequence and tests

---
+++ HW2/PrimeSequence.Tests/PrimeSequenceTests.fs
@@ -0,0 +1,21 @@
+﻿module PrimeSequenceTests
+
+open NUnit.Framework
+open FsUnit
+open PrimeSequence
+
+[<Test>]
+let primeSequence_ReturnsCorrectSeq () =
+
+    primeSequence |> Seq.take 0 |> Seq.toArray |> should equal [||]
+
+    primeSequence |> Seq.take 1 |> Seq.toArray |> should equal [|2|]
+
+    primeSequence |> Seq.take 5 |> Seq.toArray |> should equal [|2; 3; 5; 7; 11|]
+
+    primeSequence |> Seq.take 10 |> Seq.toArray |> should equal 
+        [|2; 3; 5; 7; 11; 13; 17; 19; 23; 29|]
+
+    primeSequence |> Seq.take 20 |> Seq.toArray |> should equal 
+        [|2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 
+          31; 37; 41; 43; 47; 53; 59; 61; 67; 71|]
---
+++ HW2/PrimeSequence/PrimeSequence.fs
@@ -0,0 +1,12 @@
+﻿module PrimeSequence
+
+let isPrime n =
+    if n < 2 then false
+    else
+        let upper = int (sqrt (float n))
+        seq { 2 .. upper }
+        |> Seq.forall (fun d -> n % d <> 0)
+
+let primeSequence =
+    Seq.initInfinite (fun i -> i + 2)
+    |> Seq.filter isPrime
