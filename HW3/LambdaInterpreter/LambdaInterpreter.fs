d37c134595ca20ab36acc9b897a977f5e77de3bc
khusainovilas <khusainovilas@yandex.ru>
2026-03-31 20:11:26 +0300

    fix: fix issues from code review

--- HW3/LambdaInterpreter/LambdaInterpreter.fs
+++ HW3/LambdaInterpreter/LambdaInterpreter.fs
@@ -11,32 +11,26 @@ let rec freeVariables expression =
     | Lam (name, body) -> Set.remove name (freeVariables body)
     | App (func, arg) -> Set.union (freeVariables func) (freeVariables arg)
 
-let rec renameVar oldName newName expression =
-    match expression with
-    | Var name ->
-        if name = oldName then Var newName
-        else Var name
-
-    | Lam (name, body) ->
-        if name = oldName then Lam (newName, body)
-        else Lam (name, renameVar oldName newName body)
-
-    | App (func, arg) ->
-        App (renameVar oldName newName func, renameVar oldName newName arg)
+let rec generateFreshName baseName forbidden =
+    if Set.contains baseName forbidden then
+        generateFreshName (baseName + "'") forbidden
+    else
+        baseName
 
 let rec substitute expression variableName value =
     match expression with
-    | Var name ->
-        if name = variableName then value
-        else Var name
+    | Var name when name = variableName -> value
+    | Var _ -> expression
 
+    | Lam (name, body) when name = variableName -> Lam (name, body)
     | Lam (name, body) ->
-        if name = variableName then
-            Lam (name, body)
-        elif Set.contains name (freeVariables value) then
-            let newName = name + "'"
-            let renamedBody = renameVar name newName body
-            Lam (newName, substitute renamedBody variableName value)
+        let bodyFree = freeVariables body
+        let valueFree = freeVariables value
+        let forbidden = Set.union bodyFree valueFree
+        if Set.contains name valueFree && Set.contains variableName bodyFree then
+            let newName = generateFreshName (name + "'") forbidden
+            let newBody = substitute body name (Var newName)
+            Lam (newName, substitute newBody variableName value)
         else
             Lam (name, substitute body variableName value)
 
@@ -45,25 +39,11 @@ let rec substitute expression variableName value =
 
 let rec reduce expression =
     match expression with
-    | App (Lam (param, body), arg) ->
-        reduce (substitute body param arg)
-
-    | App (func, arg) ->
-        let reducedFunc = reduce func
-        if reducedFunc <> func then
-            App (reducedFunc, arg)
-        else
-            let reducedArg = reduce arg
-            if reducedArg <> arg then
-                App (func, reducedArg)
-            else
-                expression
-
-    | Lam (param, body) ->
-        let reducedBody = reduce body
-        if reducedBody <> body then
-            Lam (param, reducedBody)
-        else
-            expression
-
     | Var _ -> expression
+    | Lam (param, body) -> Lam (param, reduce body)
+    | App (Lam (param, body), arg) ->
+        reduce (substitute body param (reduce arg))
+    | App (func, arg) ->
+        let f = reduce func
+        let a = reduce arg
+        App(f, a)
