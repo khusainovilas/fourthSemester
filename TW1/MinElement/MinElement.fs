d7e5a27c2bb13d05b258fdc894794393e146ce11
khusainovilas <khusainovilas@yandex.ru>
2026-04-02 15:36:26 +0300

    implement min element and tests

---
+++ TW1/MinElement/MinElement.fs
@@ -0,0 +1,9 @@
+﻿module MinElement
+
+let findMin list =
+    match list with
+    | [] -> None
+    | head :: tail ->
+        tail
+        |> List.fold (fun acc x -> if x < acc then x else acc) head
+        |> Some
---
+++ TW1/MinElementTests/MinElementTests.fs
@@ -0,0 +1,29 @@
+﻿module MinElementTests
+
+open NUnit.Framework
+open FsUnit
+open MinElement
+
+[<Test>]
+let FindMin_ListWithPositiveNumbers_ShouldReturnMin() =
+    let result = findMin [3; 1; 5; 2]
+
+    result |> should equal (Some 1)
+
+[<Test>]
+let FindMin_ListWithNegativeNumbers_ShouldReturnMin() =
+    let result = findMin [-3; -1; -5; -2]
+
+    result |> should equal (Some -5)
+
+[<Test>]
+let FindMin_ListWithSingleElement_ShouldReturnThatElement() =
+    let result = findMin [10]
+
+    result |> should equal (Some 10)
+
+[<Test>]
+let FindMin_EmptyList_ShouldReturnNone() =
+    let result = findMin []
+
+    result |> should equal None
